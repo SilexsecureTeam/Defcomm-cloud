1e571e3fc7f36e8d85b9e31b4857d5039cfc6546
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-02-25 17:10:41 +0100

    speed insight monitoring

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,14 +6,13 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAE0lbBhXVBZOn03ZQnYhpVqx_YdJ22474",
-  authDomain: "mayrahkee-third-party-service.firebaseapp.com",
-  projectId: "mayrahkee-third-party-service",
-  storageBucket: "mayrahkee-third-party-service.appspot.com",
-  messagingSenderId: "593408440155",
-  appId: "1:593408440155:web:334514b19379889c3b8c7e",
-  databaseUrl:
-    "https://console.firebase.google.com/u/0/project/mayrahkee-third-party-service/database/mayrahkee-third-party-service-default-rtdb/data/~2F",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  databaseUrl: "",
 };
 
 // Initialize Firebase
