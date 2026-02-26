9cf6c0d00792359932eb7a58dbf9d67787693e62
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-02-26 13:09:14 +0100

    vercel IP config

--- api/proxy/[...path].ts
+++ api/proxy/[...path].ts
@@ -25,7 +25,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Forward headers, but override host and add internal secret
   const headers: any = {
     ...req.headers,
-    host: new URL(REAL_BACKEND_URL).host,
     "X-Api-Signature": BACKEND_SECRET,
   };
 
